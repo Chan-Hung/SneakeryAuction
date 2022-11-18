b317a0f7a0b24eb5029c0de67396d56812231827
Chan-Hung <hungkevin19122001@gmail.com>
2022-11-18 20:27:58 +0700

    Change to Hostinger Database

---
+++ src/main/java/com/hung/sneakery/services/CountdownService.java
@@ -0,0 +1,16 @@
+package com.hung.sneakery.services;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class CountdownService {
+
+  @Scheduled(fixedDelay = 2000)
+  public void scheduleFixedDelayTask() throws InterruptedException{
+      System.out.println("Task 1 - " + new Date());
+  }
+
+}
