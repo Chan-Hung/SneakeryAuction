f3f24bc636b5ffc0f0a63452f624bab14f905a6c
Chan-Hung <hungkevin19122001@gmail.com>
2023-01-04 17:07:12 +0700

    Config Swagger

--- src/main/java/com/hung/sneakery/SneakeryApplication.java
+++ src/main/java/com/hung/sneakery/SneakeryApplication.java
@@ -3,15 +3,9 @@ package com.hung.sneakery;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
-@EnableWebMvc
-@EntityScan(basePackages = "com.hung.sneakery.data.models.entities")
 public class SneakeryApplication {
     public static void main(String[] args) {
         SpringApplication.run(SneakeryApplication.class, args);
