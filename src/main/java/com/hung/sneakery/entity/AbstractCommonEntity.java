3e5a1b14202d85da35aba5d9138cec0fc52bdb52
Hung Doan Chan <hungkevin19122001@gmail.com>
2023-12-17 23:49:03 +0700

    Automatically create date time

--- src/main/java/com/hung/sneakery/entity/AbstractCommonEntity.java
+++ src/main/java/com/hung/sneakery/entity/AbstractCommonEntity.java
@@ -3,13 +3,16 @@ package com.hung.sneakery.entity;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 public abstract class AbstractCommonEntity implements Serializable {
 
