53ea08d5f07f2fba17fda8163181c8127cdece2c
Hung Doan Chan <59687483+Chan-Hung@users.noreply.github.com>
2024-03-10 20:17:43 +0700

    Update Typedef

--- src/main/java/com/hung/sneakery/entity/Category.java
+++ src/main/java/com/hung/sneakery/entity/Category.java
@@ -1,7 +1,10 @@
 package com.hung.sneakery.entity;
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.*;
 import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -15,6 +18,7 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@TypeDefs({@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)})
 public class Category extends AbstractCommonEntity {
 
     @Id
