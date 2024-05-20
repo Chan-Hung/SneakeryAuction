1ef8b57ace7f8086a718de89df32314071591c7d
Hung Doan Chan <59687483+Chan-Hung@users.noreply.github.com>
2024-05-20 22:38:00 +0700

    Hot fix Swagger not accessible

--- src/main/java/com/hung/sneakery/config/security/WebSecurityConfig.java
+++ src/main/java/com/hung/sneakery/config/security/WebSecurityConfig.java
@@ -96,6 +96,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/products/**").permitAll()
                 .antMatchers("/categories/**").permitAll()
                 .antMatchers("/bid-history/**").permitAll()
+                .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs", "/webjars/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
